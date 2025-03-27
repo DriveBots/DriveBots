74107ab22f420d09a37c34d9875144363dcab3a1
width <abc23-018@thuto.bac.ac.bw>
2025-03-27 10:46:18 +0200

    Added linked pages and created the links.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,45 @@ namespace DriveBots.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View(); //Looks for "Login.cshtml" in Views/Home/
+        }
+
+        public IActionResult SignUp()
+        {
+            return View(); //Looks for "SignUp.cshtml" in Views/Home/
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View(); //Looks for "ContactUs.cshtml" in Views/Home/
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View(); //Looks for "Services.cshtml" in Views/Home/
+        }
+        public IActionResult Services()
+        {
+            return View(); //Looks for "Services.cshtml" in Views/Home/
+        }
+        
+        public IActionResult ApplicationPage()
+        {
+            return View(); //Looks for "ApplicationPage.cshtml" in Views/Home/
+        }
+
+        public IActionResult Applicant()
+        {
+            return View(); //Looks for "Applicant.cshtml" in Views/Home/
+        }
+
+        public IActionResult Admin()
+        {
+            return View(); //Looks for "Admin.cshtml" in Views/Home/
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
