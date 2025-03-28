b03f0e76d694bffe95bd2d3e56eaa2e5399896ee
width <abc23-018@thuto.bac.ac.bw>
2025-03-28 21:37:07 +0200

    Attempted backend for the login page

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DriveBots.Models
 {
     public class ErrorViewModel
@@ -5,5 +7,21 @@ namespace DriveBots.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
