af00693fe1a05aa0afb773d251c0e69ef7c25848
width <abc23-018@thuto.bac.ac.bw>
2025-04-20 23:03:00 +0200

    Added a default admin account and migrations

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DriveBots.Models;
+
+namespace DriveBots.Controllers
+{
+    public class SignUpController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Applicant", "Home"); // Redirect after successful signup
+            }
+
+            return View(model);
+        }
+    }
+}
